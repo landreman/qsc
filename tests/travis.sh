cbfc833fb66f088a25f1e78a293ba88e9f63e5a1
Matt Landreman <mattland@umd.edu>
2020-04-20 12:23:18 -0400

    Trying again to get Travis-CI working

--- tests/travis.sh
+++ tests/travis.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-set -ex
+#set -ex
+set -e
 # In the above line, "set -x" causes this script to exit as soon as any line fails. "set -e" causes each line of this script to be printed (with a + in front) before it is executed, so if a step fails, you can see from the travis log what command failed.
 
 echo Hello from travis.sh
@@ -10,7 +11,7 @@ pwd
 ls
 env
 which mpicc
-#which mpiexec
+which mpiexec
 which mpirun
 
 cmake --version
@@ -21,4 +22,4 @@ make
 
 bin/qsc_driver
 
-mpirun -n 2 bin/qsc_driver
+mpiexec -n 2 bin/qsc_driver
