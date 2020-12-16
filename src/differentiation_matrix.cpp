448417ab7f201241db6f516db892d300e856a166
Matt Landreman <mattland@umd.edu>
2020-12-16 11:47:56 -0500

    Adding differentiation matrix tests

--- src/differentiation_matrix.cpp
+++ src/differentiation_matrix.cpp
@@ -1,6 +1,5 @@
 #include <cmath>
 #include "qsc.hpp"
-#include "vector_matrix.hpp"
 
 using namespace qsc;
   
@@ -14,13 +13,28 @@ Matrix qsc::differentiation_matrix(const int N, const QSC_REAL xmin, const QSC_R
   Vector topc(n2);
   Vector col1(N);
   Matrix ddx(N, N);
-  // Matrix ddx = new Matrix(N, N);
   col1[0] = 0.0;
   int j, k;
     
   if (N % 2 == 0) {
+    // Even size:
+    
     for (j = 0; j < n2; j++) {
+      // Coefficient in the next line is 1.0 in DMSuite:fourdif but 0.5 in sfincs/quasisymmetry.
       topc[j] = 0.5 / tan((j + 1) * h / 2);
+    }
+    for (j = 1; j < n2 + 1; j++) {
+      col1[j] = topc[j - 1];
+    }
+    for (j = 0; j < n1; j++) {
+      col1[n2 + 1 + j] = -topc[n1 - 1 - j];
+    }
+    
+  } else {
+    // Odd size:
+    
+    for (j = 0; j < n2; j++) {
+      topc[j] = 0.5 / sin((j + 1) * h / 2);
     }
     for (j = 1; j < n2 + 1; j++) {
       col1[j] = topc[j - 1];
@@ -28,22 +42,20 @@ Matrix qsc::differentiation_matrix(const int N, const QSC_REAL xmin, const QSC_R
     for (j = 0; j < n1; j++) {
       col1[n2 + 1 + j] = topc[n1 - 1 - j];
     }
-    for (j = 1; j < N; j += 2) {
-      col1[j] = -col1[j];
+  }
+  
+  for (j = 1; j < N; j += 2) {
+    col1[j] = -col1[j];
+  }
+  col1 = 2 * pi / (xmax - xmin) * col1;
+
+  // Create a Toeplitz matrix:
+  for (j = 0; j < N; j++) {
+    for (k = j; k < N; k++) {
+      ddx(j, k) = -col1[k - j];
     }
-    col1 = 2 * pi / (xmax - xmin) * col1;
-    // Create a Toeplitz matrix:
-    for (j = 0; j < N; j++) {
-      for (k = j; k < N; k++) {
-	ddx(j, k) = -col1[k - j];
-      }
-      for (k = 0; k < j; k++) {
-	ddx(j, k) = col1[j - k];
-      }
-    }
-  } else {
-    for (j = 0; j < n2; j++) {
-      topc[j] = 0.5 / sin((j + 1) * h / 2);
+    for (k = 0; k < j; k++) {
+      ddx(j, k) = col1[j - k];
     }
   }
   
--- src/tests/differentiation_matrix_tests.cpp
+++ src/tests/differentiation_matrix_tests.cpp
@@ -18,3 +18,71 @@ TEST_CASE("2x2 differentiation matrix") {
     }
   }
 }
+
+TEST_CASE("3x3 differentiation matrix") {
+  int n = 3;
+  
+  Matrix D1(n, n);
+  D1 = 0.0;
+  double x = 0.577350269189626;
+  D1(1, 0) = -x;
+  D1(2, 1) = -x;
+  D1(0, 2) = -x;
+  D1(0, 1) = x;
+  D1(1, 2) = x;
+  D1(2, 0) = x;
+
+  Matrix D2 = differentiation_matrix(n, 0, 2 * pi);
+
+  for (int j = 0; j < n; j++) {
+    for (int k = 0; k < n; k++) {
+      CHECK(D1(k, j) == Approx(D2(k, j)));
+    }
+  }
+}
+
+TEST_CASE("4x4 differentiation matrix") {
+  int n = 4;
+  
+  Matrix D1(n, n);
+  D1 = 0.0;
+  D1(1, 0) = -0.5;
+  D1(2, 1) = -0.5;
+  D1(3, 2) = -0.5;
+  D1(0, 3) = -0.5;
+  D1(0, 1) = 0.5;
+  D1(1, 2) = 0.5;
+  D1(2, 3) = 0.5;
+  D1(3, 0) = 0.5;
+
+  Matrix D2 = differentiation_matrix(n, 0, 2 * pi);
+  
+  for (int j = 0; j < n; j++) {
+    for (int k = 0; k < n; k++) {
+      CHECK(D1(k, j) == Approx(D2(k, j)));
+    }
+  }
+}
+
+TEST_CASE("5x5 differentiation matrix") {
+  int n = 5;
+  
+  Matrix D1(n, n);
+  D1 = 0.0;
+  double e = 0.85065080835204;
+  double f = 0.525731112119134;
+  for (int j = 0; j < n; j++) {
+    D1(j, (j + 1) % n) = e;
+    D1(j, (j + 2) % n) = -f;
+    D1(j, (j + 3) % n) = f;
+    D1(j, (j + 4) % n) = -e;
+  }
+  
+  Matrix D2 = differentiation_matrix(n, 0, 2 * pi);
+
+  for (int j = 0; j < n; j++) {
+    for (int k = 0; k < n; k++) {
+      CHECK(D1(k, j) == Approx(D2(k, j)));
+    }
+  }
+}
