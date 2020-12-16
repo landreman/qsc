7d0ff5e94f6ce6a29f481b651073f1d13cfc9b6e
Matt Landreman <mattland@umd.edu>
2020-12-16 14:13:52 -0500

    Added BLAS matrix-vector multiply

--- src/vector_matrix.cpp
+++ src/vector_matrix.cpp
@@ -3,6 +3,20 @@
 #include <cassert>
 #include "vector_matrix.hpp"
 
+// Representation of BLAS and LAPACK routines we need:
+extern "C" {
+  // Matrix-vector multiply:
+  void dgemv_(char* TRANS, int* M, int* N, double* ALPHA, double* A, int* LDA,
+	      double* X, int* INCX, double* BETA, double* Y, int* INCY);
+
+  // Matrix-matrix multiply:
+  // void dgemm_(char* TRANSA, char* TRANSB, int* M, int* N, int* K, double* ALPHA,
+  //	      double* A, int* LDA, double* B, int* LDB, double* BETA, double* C, int* LDC);
+
+  // Solve linear system:
+  // void dgesv_(int* N, int* NRHS, double* A, int* LDA, int* IPIV, double* B, int* LDB, int* INFO);
+}
+
 using namespace qsc;
 
 Matrix::Matrix(index_type nrows_in, index_type ncols_in)
@@ -35,11 +49,24 @@ std::ostream& qsc::operator<< (std::ostream& os, Matrix& m) {
 Vector qsc::operator*(Matrix& m, Vector& v) {
   assert(m.ncols() == v.size());
   Vector result(m.nrows());
+
+  /*
+  // Non-BLAS simple-minded method:
   double sum = 0;
   for (int j = 0; j < m.nrows(); j++) {
     sum = 0;
     for (int k = 0; k < v.size(); k++) sum += m(j, k) * v[k];
     result[j] = sum;
   }
+  */
+
+  char TRANS = 'N';
+  int INC = 1;
+  int nrows = m.nrows();
+  int ncols = m.ncols();
+  double ALPHA = 1.0;
+  double BETA = 0.0;
+  dgemv_(&TRANS, &nrows, &ncols, &ALPHA, &m(0, 0), &nrows,
+	      &v[0], &INC, &BETA, &result[0], &INC);
   return result;
 }
