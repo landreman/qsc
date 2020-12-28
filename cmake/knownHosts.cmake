b0990329e4a07e68b21b1b98582ad3520670dd3c
Matt Landreman <mattland@umd.edu>
2020-12-28 13:21:05 -0500

    Swapped Travis for Github Actions in known hosts

--- cmake/knownHosts.cmake
+++ cmake/knownHosts.cmake
@@ -7,17 +7,16 @@ message("hostname is ${HOST}")
 message("fully qualified domain name is ${FQDN}")
 
 
-if(DEFINED ENV{TRAVIS_ARCH})
-  message("Detected host is Travis-CI")
-  # For Travis-CI, the flags "--mca btl_base_warn_component_unused 0 --mca orte_base_help_aggregate 0" are added to mpiexec
-  # to avoid some warning messages.
-  set(QSC_COMMAND_TO_SUBMIT_JOB "mpiexec -n NUM_PROCS --mca btl_base_warn_component_unused 0 --mca orte_base_help_aggregate 0")
+if(DEFINED ENV{GITHUB_ACTIONS})
+  message("Detected host is Github Actions CI")
+  # Some flags are added to mpiexec to avoid some warning messages.
+  set(QSC_COMMAND_TO_SUBMIT_JOB "mpiexec -n NUM_PROCS --mca btl_base_warn_component_unused 0")
+  #set(QSC_COMMAND_TO_SUBMIT_JOB "mpiexec -n NUM_PROCS --mca btl_base_warn_component_unused 0 --mca orte_base_help_aggregate 0")
 
 elseif(DEFINED ENV{NERSC_HOST})
   message("Detected host is NERSC Cori")
   set(CMAKE_CXX_COMPILER "CC")
 
-#elseif((DEFINED ENV{CLUSTER}) AND ($ENV{CLUSTER} STREQUAL "DRACO"))
 elseif("$ENV{CLUSTER}" STREQUAL "DRACO")
   message("Detected host is IPP Draco")
   # CMake detects gnu compiler unless you specify Intel:
